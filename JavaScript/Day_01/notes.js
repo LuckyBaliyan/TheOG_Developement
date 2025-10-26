c954ae26eb2be04d2718d222617bab5c2f4600c7
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-10-26 22:22:56 +0530

    JS day(01) intro to js and its evolutions and uses and run

---
+++ JavaScript/Day_01/notes.js
@@ -0,0 +1,74 @@
+/*
+- Java Script - scripting language
+
+-Js is developed by brandon eich around 15-16 days 
+
+  thats why it consists many bugs initially in order to fix them and enhance the js 
+  there were many versions released in  over  a past years and a biggest version is called 
+  ES-6 was the biggest change in js histroy because it changes almost completely bhot saare new 
+  versions deye unhone.
+
+- what is a scripting language why its diffrent from programming languages and in which category does js lies
+
+   A scripting language is a type of programming language that is usually interpreted rather than compiled.
+   That means the code is executed line by line at runtime by an interpreter 
+   (e.g., Python interpreter, Node.js engine, etc.), 
+   instead of being converted into machine code ahead of time like compiled languages (C, C++, Rust, etc.).
+   
+   Scripting languages are often used to:
+   
+   Automate repetitive tasks
+   Glue or “script” together other program.
+   Create web pages, system scripts, or simple utilities quickly
+   Examples: JavaScript, Python, PHP, Perl, Ruby, Bash
+
+- Programming Language vs Scripting Language
+
+  Feature	  Programming Language	                Scripting Language
+
+  Compilation	Usually compiled before execution	 Usually interpreted at runtime
+  Execution     Speed	Faster (machine code)	     Slower (interpreted)
+  Use Case	    Building full applications,software  Automating tasks, web scripting, data processing
+  Examples	    C, C++, Java, Rust	                 JavaScript, Python, Bash, PHP
+  
+  Note: All scripting languages are programming languages, but not all programming languages are scripting languages.
+  The main difference lies in how and where the code runs, not in what it can do.
+
+- JavaScript is primarily a scripting language — it was originally designed to add interactivity and automation to web pages (in browsers).
+  However, with modern engines like Node.js, JS is now also used as a general-purpose programming language, capable of building:
+  
+  Backend servers (Node.js, Deno)
+  Mobile apps (React Native)
+  Desktop apps (Electron)
+  Even AI tools and automation scripts
+  
+  So today, JavaScript is both a scripting and programming languagez, 
+  depending on context — but traditionally, it’s considered a scripting language.
+
+- JS a general browser Language.
+
+  Js was initially developed for the browsers only and to avoid the mahyem of  diffrent browsers using there language
+  only so a community was built names ekmaScript whenever a new version of js came we verify it to use or not and second
+  every browser should be using js from now on.
+
+- Setup 
+
+  We connect js file with our index.html and whenever the browser load out html file the js file also loads.
+
+- variables 
+
+  var - before ES-6
+  var,let,const after ES-6
+
+- Ways to print on console 
+  
+  console.log()
+  console.warn()
+  console.error()
+
+- 
+*/
+
+const name = "Avi";
+console.log(name);
+
---
+++ JavaScript/Day_01/script.js
@@ -0,0 +1,5 @@
+// Hello to html page
+document.writeln("Hello From Js");
+
+//Popup alert 
+alert("Hello from JS");
