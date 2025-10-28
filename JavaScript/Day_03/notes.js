fd05758e769897412cc1dbabce6f97d31d0ca9ab
Lucky Baliyan <baliyanlucky85@gmail.com>
2025-10-28 21:48:23 +0530

    Day(03) js dataTypes

---
+++ JavaScript/Day_03/notes.js
@@ -0,0 +1,42 @@
+// comments in js
+
+/* 
+Comments if we write anything then js find it and if its not defined in js so js will 
+through an error to avoid it we use comments to write some info with peice of code
+*/
+
+// Data types in js
+
+/*
+
+  float- 12.3
+  number - 12 
+  String - h or hrr
+  ch - h
+  boolean - true or flase
+  null - jab kuch value dene ko nahi ho to null dete h  or aage data milega 
+  undefined  - a variable  without value is declared
+  NAN - not a number generates when there is a operation held on a variable that makes no scence
+        or may be not a valid operation for the number.
+  Objects - arrays and objects 
+  arrays - homogenious mixture of datas in js we can literally use anything in same array.
+
+*/
+  
+// Arrays
+ex = ["We",1,2.3,NaN,undefined,{},()=>log("hi")];
+
+// NaN
+console.log(12*"Mu");
+
+// Infinity
+
+console.log(1/0);
+
+
+//Primitive values :- values which are not wrapped in any brackets in js  ye hote h jinki value direct copy krte h.
+//Refrenced values:- jinki value direct ni kr skte ho just unka refrence copy ho jata h.
+
+console.log(0*2/0);
+
+
