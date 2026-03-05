5b1f522a601590d820e1c3d39de48b94846895b6
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-03-05 15:16:29 +0530

    Getting Started with backend

---
+++ BackEnd/Concepts/day1/index.js
@@ -0,0 +1,17 @@
+//console.log('starting with node js to run write node filename.js in terminal');
+
+//Lets use Our first npm package
+//import catMe from "cat-me";
+
+const catMe = require("cat-me");
+
+// acc to documentation this function return a random cat
+//with no params passed
+console.log(catMe()); 
+
+//if we pass a name 
+
+console.log(catMe('nyan'));  //gives a prebuilt nayan cat
+
+
+
---
+++ BackEnd/Concepts/day1/startingServer/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const server = express(); //server is created with this call but not started yet
+//created a server instance (obj) with this 
+
+server.get('/',(req,res)=>{
+    res.send("Hello HomePage!");
+});
+
+server.get("/about",function(req,res){
+    res.send("About Page!");
+});
+
+server.listen(3000); // server ko start karta hai 
