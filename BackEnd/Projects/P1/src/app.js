aac1d4ad48904bd8203256d9965cf436e3fe4de8
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-03-07 16:31:50 +0530

    Status codes and started with Mongodb atlas and mongodb compass application Installation

--- BackEnd/Projects/P1/src/app.js
+++ BackEnd/Projects/P1/src/app.js
@@ -19,12 +19,22 @@ app.post("/notes",(req,res)=>{
 
     notes.push(req.body);
     console.log(notes);
-    res.send("note Created!!");
+    //res.send("note Created!!");
+
+    res.status(201).json({
+        message:"Note created Sucessfully!"
+    });
+
 });
 
 //get all the current notes
 app.get("/notes",(req,res)=>{
-    res.send(notes);
+    //res.send(notes);
+
+    res.status(200).json({
+        notes:notes
+    });
+
 })
 
 //delete note 
@@ -32,7 +42,11 @@ app.delete("/notes/:index",(req,res)=>{
     console.log(req.params.index);
     delete notes[req.params.index];
 
-    res.send("Note Deleted!");
+    //res.send("Note Deleted!");
+    res.status(204).json({
+        message:"Note Deleted Sucessfully!"
+    })
+
 });
 
 //PATCH Notes/:index
@@ -40,8 +54,14 @@ app.delete("/notes/:index",(req,res)=>{
 app.patch("/notes/:index",(req,res)=>{
     notes[req.params.index].description = req.body.description;
 
-    res.send("Note Updated Sucessfully!");
-})
+    //res.send("Note Updated Sucessfully!");
+
+    res.status(200).json({
+        message:"Note Updated!",
+        updtaedNote:notes[req.params.index],
+    })
+
+});
 
 module.exports = app;
 
