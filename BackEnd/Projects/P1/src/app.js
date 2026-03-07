17f93093ebb709345d8c4567e4f4e28022db2add
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-03-07 13:18:54 +0530

    Project1 day2 understanding all get post patch and delete methods of restApi's

---
+++ BackEnd/Projects/P1/src/app.js
@@ -0,0 +1,47 @@
+/* Create and config the server */
+
+const express = require("express");
+const app = express();
+
+
+app.use(express.json());//middleware from express
+
+const notes = [];
+
+// create a Note
+app.post("/notes",(req,res)=>{
+    console.log(req.body); 
+    //will get undefined when sended from postman in JSON format
+
+    //but why undefined because by default the express alone is't enought to persist 
+    //JSON format data so we need to use a middleware by express
+    //server.use(express.json());
+
+    notes.push(req.body);
+    console.log(notes);
+    res.send("note Created!!");
+});
+
+//get all the current notes
+app.get("/notes",(req,res)=>{
+    res.send(notes);
+})
+
+//delete note 
+app.delete("/notes/:index",(req,res)=>{
+    console.log(req.params.index);
+    delete notes[req.params.index];
+
+    res.send("Note Deleted!");
+});
+
+//PATCH Notes/:index
+//req.body = {description:"simple modified description"}
+app.patch("/notes/:index",(req,res)=>{
+    notes[req.params.index].description = req.body.description;
+
+    res.send("Note Updated Sucessfully!");
+})
+
+module.exports = app;
+
