b687cb58f59f0b8b90195201664d04843a82548c
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-03-11 21:37:33 +0530

    added env to gitignore  and change in db config

--- BackEnd/Projects/P1/Backend/src/config/database.js
+++ BackEnd/Projects/P1/Backend/src/config/database.js
@@ -1,9 +1,9 @@
 const mongo = require("mongoose");
 
-const uri = process.env.MONGO_URI;
+const url = process.env.MONGO_URI;
 
 function connectDb(){
-    mongo.connect(uri).then(()=>{console.log("Connected DB!")});
+    mongo.connect(url).then(()=>{console.log("Connected DB!")});
 }
 
 module.exports = connectDb;
