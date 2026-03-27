b816d945430a8ba2eef963171b5b0c91854ab36f
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-03-28 01:37:33 +0530

    Login Api and Password Hashing

--- BackEnd/Concepts/Authentication_4/src/app.js
+++ BackEnd/Concepts/Authentication_4/src/app.js
@@ -1,11 +1,17 @@
 
 const express = require("express");
 const authRouter = require("./routes/auth.routes");
+const cookieParser = require("cookie-parser");
+
 
 const app = express();
 app.use(express.json()); //middleware to pass data in req.body
+app.use(cookieParser()); 
 
 //now we have to hit this api/auth/register to call
-app.use("/api/auth/register",authRouter);
+app.use("/api/auth",authRouter);
+app.use("/api/auth",authRouter);
+app.use("/api/auth",authRouter);
+
 
 module.exports = app;
--- BackEnd/Concepts/Authentication_4/src/routes/auth.routes.js
+++ BackEnd/Concepts/Authentication_4/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const userModel = require("../models/user.model");
 const jwt  = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const authRouter = express.Router();
 
@@ -9,7 +10,7 @@ const authRouter = express.Router();
 */
 
 //creating api for registration
-authRouter.post("/",async (req,res)=>{
+authRouter.post("/register",async (req,res)=>{
     const {name, email, password} = req.body;
 
     const isUserAlreadyExsist = await userModel.findOne({email});
@@ -20,7 +21,9 @@ authRouter.post("/",async (req,res)=>{
         })
     }
 
-    const user = await userModel.create({name, email, password});
+    const hashPass = crypto.createHash("md5").update(password).digest("hex");
+
+    const user = await userModel.create({name, email, password:hashPass});
 
     const token = jwt.sign(
         {
@@ -36,6 +39,64 @@ authRouter.post("/",async (req,res)=>{
         message:"registered sucessfully!",
         user,
         token
+    });
+
+})
+
+/**
+ * /api/auth/protected
+*/
+
+authRouter.post("/protected",(req,res)=>{
+    const token = req.cookies;
+    console.log(req.cookies);
+
+    res.status(200).json({
+        message:"sucess from protected api",
+        token
+    })
+});
+
+/**
+ * Login Api:-  /api/auth/login
+*/
+
+authRouter.post("/login",async (req, res)=>{
+    const {email, password} = req.body;
+
+    const user = await userModel.findOne({email});
+
+    if(!user){
+        return res.status(404).json({
+            message:"user Not found at this email"
+        })
+    }
+
+    const hashPass = crypto.createHash("md5").update(password).digest("hex");
+
+    const isPass = user.password === hashPass;
+    
+    if(!isPass){
+        return res.status(404).json({
+            message:"Entered Password is incorrect not matching!"
+        })
+    }
+
+    //if user found make a new token again on each sucessfull login always 
+    const token = jwt.sign(
+        {
+        id:user._id,
+        email:user.email
+        },
+        process.env.JWT_SECRET
+    );
+
+    res.cookie("jwt_token", token);
+
+    return res.status(200).json({
+        message:"Sucessfully logged In!",
+        user:user,
+        Usertoken:token
     });
 
 })
