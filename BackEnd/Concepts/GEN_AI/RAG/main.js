08c82b1a936337a4dc7470b0c5cc86934fd6afed
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-08-15 00:51:17 +0530

    feat: implement RAG pipeline with Mistral embeddings and Pinecone vector storage

--- BackEnd/Concepts/GEN_AI/RAG/main.js
+++ BackEnd/Concepts/GEN_AI/RAG/main.js
@@ -3,6 +3,7 @@ import pdfParse from "pdf-parse";
 import fs from "fs";
 import { MistralAIEmbeddings } from "@langchain/mistralai";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { Pinecone } from "@pinecone-database/pinecone";
 
 const buffer = fs.readFileSync("./story.pdf");
 
@@ -14,6 +15,12 @@ const embeddings = new MistralAIEmbeddings({
       model: "mistral-embed", // Default Mistral embedding model
 });
 
+//vector db setup
+const pc = new Pinecone({
+      apiKey: process.env.PINECONE_API_KEY
+});
+
+const index = pc.Index("rag-part1");
 
 const splitter = new RecursiveCharacterTextSplitter({
       chunkSize: 100, //count the words
@@ -21,9 +28,38 @@ const splitter = new RecursiveCharacterTextSplitter({
 })
 
 const chunks = await splitter.splitText(data.text);
-//to avoid too many inputs
-const sliced = chunks.slice(0, 90);
-console.log(sliced);
 
-const docs = await embeddings.embedDocuments(sliced);
-console.log(docs);
+//make a proper response of all chuncks and related embedding
+const docs = await Promise.all(chunks.map(async (chunk) => {
+      const embedding = await embeddings.embedQuery(chunk)
+      return {
+            text: chunk,
+            embedding
+      }
+}));
+
+//store in vector db where metaData is the plain text value
+const result = await index.upsert({
+      records: docs.map((doc, _id) => ({
+            id: `doc-${_id}`,
+            values: doc.embedding,
+            metadata: {
+                  text: doc.text
+            }
+      }))
+});
+
+console.log(result);
+
+//query in  vector Db
+const queryEmbedding = await embeddings.embedQuery("Conclude this story");
+
+//fetch top k similar results based on similarity of embeddings
+const queryResult = await index.query({
+      vector: queryEmbedding,
+      topK: 5,
+      includeMetadata: true,
+})
+
+console.log(queryResult);
+
